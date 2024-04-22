4e526d5508875f9c0c7bab3aa0509e09741a9e70
Esteban Durango <Esteban.durangov@udea.edu.co>
2024-04-22 14:42:30 -0500

    fix name on AirplaneModel

--- src/main/java/co/edu/udea/sitas/domain/model/AirplaneModel.java
+++ src/main/java/co/edu/udea/sitas/domain/model/AirplaneModel.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class AirplaneModel implements Serializable {
     @Id
     @Column(name = "airplane_model")
-    private String model;
+    private String airplaneModel;
 
     @Column(name = "family")
     private String family;
@@ -42,7 +42,7 @@ public class AirplaneModel implements Serializable {
                 ", cargoCapacity=" + cargoCapacity +
                 ", capacity=" + capacity +
                 ", family='" + family + '\'' +
-                ", airplaneModel='" + model + '\'' +
+                ", airplaneModel='" + airplaneModel + '\'' +
                 '}';
     }
 }
