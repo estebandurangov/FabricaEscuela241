673480d5d7d4b668057bd23b0478eead51fd1660
Esteban Durango <Esteban.durangov@udea.edu.co>
2024-04-22 19:47:28 -0500

    add findAll test

--- src/test/java/FlightDAOJPATest.java
+++ src/test/java/FlightDAOJPATest.java
@@ -1,5 +1,19 @@
+import co.edu.udea.sitas.domain.dto.FlightDTO;
+import co.edu.udea.sitas.controllers.v1.FlightController;
+import co.edu.udea.sitas.domain.model.Flight;
+import co.edu.udea.sitas.services.FlightService;
+import static org.mockito.Mockito.*;
+
+import co.edu.udea.sitas.services.ScaleService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,8 +25,21 @@ class FlightDAOJPATest {
 
     @Test
     void findAll() {
-        Integer num = 2;
-        assertEquals(2, num);
+        // Arrange
+        FlightService flightServiceMock = mock(FlightService.class);
+        ScaleService scaleServiceMock = mock(ScaleService.class);
+        FlightController flightController = new FlightController(flightServiceMock, scaleServiceMock);
+        Map<String, String> reqParam = new HashMap<>();
+        reqParam.put("param1", "value1");
+        reqParam.put("param2", "value2");
+        List<Flight> mockFlights = List.of(new Flight(), new Flight()); // Mocked flights
+        when(flightServiceMock.findAll(reqParam)).thenReturn(mockFlights);
+
+        // Act
+        ResponseEntity<List<FlightDTO>> responseEntity = flightController.findALL(reqParam);
+
+        // Assert
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
     }
 
     @Test
